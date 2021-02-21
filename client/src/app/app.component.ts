4c55dde0c10305dd7611ab4c580daf348700a7d2
Toka Mohamed <toka.naguib@fekretyonline.com>
2021-02-21 14:21:19 +0200

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import {IProduct} from './models/product';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent implements OnInit {
   title = 'Skinet';
-  products:any[];
-  constructor(private http:HttpClient){
-  }
+  products: any[];
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:IPagination)=>{
-      this.products=response.data;
-    }),error=>{
-      console.log(error)
-    }
-  }
-
-
-
-} 
+  ngOnInit(): void {}
+}
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,100 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
+import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shop-params';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search', { static: true }) searchTerm: ElementRef;
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  shopParams = new ShopParams();
+  totalCount: number;
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'PriceL Low to High', value: 'priceAsc' },
+    { name: 'PriceL High to Low', value: 'priceDesc' },
+  ];
+
+  constructor(private shopService: ShopService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.shopParams).subscribe(
+      (res) => {
+        this.products = res.data;
+        this.shopParams.pageNumer = res.pageIndex;
+        this.shopParams.pageSize = res.pageSize;
+        this.totalCount = res.count;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe(
+      (res) => {
+        this.brands = [{ id: 0, name: 'All' }, ...res];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe(
+      (res) => {
+        this.types = [{ id: 0, name: 'All' }, ...res];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onBrandSelected(brandId: number) {
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumer = 1;
+    this.getProducts();
+  }
+  onTypeSelected(typeId: number) {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumer = 1;
+    this.getProducts();
+  }
+  onSortSelected(sort: string) {
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+  onPageChanged(event: any) {
+    if (this.shopParams.pageNumer !== event) {
+      this.shopParams.pageNumer = event;
+      this.getProducts();
+    }
+  }
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumer = 1;
+    this.getProducts();
+  }
+  onReset() {
+    this.searchTerm.nativeElement.vlue = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}
