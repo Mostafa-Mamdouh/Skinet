18cf4ff5791bbb8760cfe40e4d1f9288bcacd8c2
Toka Mohamed <toka.naguib@fekretyonline.com>
2021-02-22 08:10:55 +0200

    End of section 11

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,11 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { NotFountComponent } from './not-fount/not-fount.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { TestErrorComponent } from './test-error/test-error.component';
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    NotFountComponent,
+    ServerErrorComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
   exports: [NavBarComponent],
 })
 export class CoreModule {}
