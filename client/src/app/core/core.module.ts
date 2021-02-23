7c805c9e34721491df5ad864ab746cb9dd171f2a
Toka Mohamed <toka.naguib@fekretyonline.com>
2021-02-23 03:49:04 +0200

    end section 12

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NotFountComponent } from './not-fount/not-fount.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { TestErrorComponent } from './test-error/test-error.component';
 @NgModule({
@@ -12,15 +14,17 @@ import { TestErrorComponent } from './test-error/test-error.component';
     TestErrorComponent,
     NotFountComponent,
     ServerErrorComponent,
+    SectionHeaderComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
   ],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, SectionHeaderComponent],
 })
 export class CoreModule {}
